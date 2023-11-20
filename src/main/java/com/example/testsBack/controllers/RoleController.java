a101d0307dfb6f945df1b1968f399de830059412
morozpr <mihsa372004@gmail.com>
2023-11-20 22:43:15 +0300

    add swagger

--- src/main/java/com/example/testsBack/controllers/RoleController.java
+++ src/main/java/com/example/testsBack/controllers/RoleController.java
@@ -4,7 +4,6 @@ import com.example.testsBack.entities.Role;
 import com.example.testsBack.exceptions.BadRequest;
 import com.example.testsBack.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
