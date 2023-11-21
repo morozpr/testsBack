bc78e4c31b134f5a76d6e8475ebf3f2992fd2404
Liraalkeyn <mikunar2018@gmail.com>
2023-11-21 20:32:27 +0300

    add UniversityService

--- src/main/java/com/example/testsBack/repositories/UniversityRepository.java
+++ src/main/java/com/example/testsBack/repositories/UniversityRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UniversityRepository extends CrudRepository<University, Long>, JpaSpecificationExecutor<University> {
+    boolean existsByUniversityName(String universityName);
 }
