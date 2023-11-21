00d2df2b1cfc4d957ff79ec91dbd657b8bf622e0
morozpr <mihsa372004@gmail.com>
2023-11-21 17:37:58 +0300

    add DisciplineService

--- src/main/java/com/example/testsBack/repositories/DisciplineRepository.java
+++ src/main/java/com/example/testsBack/repositories/DisciplineRepository.java
@@ -4,5 +4,8 @@ import com.example.testsBack.entities.Discipline;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DisciplineRepository extends CrudRepository<Discipline, Long>, JpaSpecificationExecutor<Discipline> {
+    List<Discipline> findByDisciplineName(String disciplineName);
 }
---
+++ src/main/java/com/example/testsBack/services/DisciplineService.java
@@ -0,0 +1,51 @@
+package com.example.testsBack.services;
+
+import com.example.testsBack.entities.Discipline;
+import com.example.testsBack.exceptions.BadRequest;
+import com.example.testsBack.repositories.DisciplineRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DisciplineService {
+    private final DisciplineRepository disciplineRepository;
+
+    DisciplineService(DisciplineRepository disciplineRepository) {
+        this.disciplineRepository = disciplineRepository;
+    }
+
+    public Discipline addObject(Discipline discipline) throws BadRequest {
+        List<Discipline> check = disciplineRepository.findByDisciplineName(discipline.getDisciplineName());
+        if (check != null) {
+            throw new BadRequest("Object already exist");
+        }
+        return disciplineRepository.save(discipline);
+    }
+
+    public Discipline getOneObject(Long id) throws BadRequest {
+        if (disciplineRepository.findById(id) == null) {
+            throw new BadRequest("Object not found");
+        }
+        return disciplineRepository.findById(id).get();
+    }
+
+    public Iterable<Discipline> getAllObjects() {
+        return disciplineRepository.findAll();
+    }
+
+    public Discipline editObject(Long id, Discipline discipline) throws BadRequest {
+        if (disciplineRepository.findById(id) == null) {
+            throw new BadRequest("Object not found");
+        }
+        discipline.setId(id);
+        return disciplineRepository.save(discipline);
+    }
+
+    public void deleteObject(Long id) throws BadRequest {
+        if (disciplineRepository.findById(id) == null) {
+            throw new BadRequest("Object not found");
+        }
+        disciplineRepository.deleteById(id);
+    }
+}
