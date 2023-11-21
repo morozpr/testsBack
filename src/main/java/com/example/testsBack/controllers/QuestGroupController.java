fa595c88a18f41b041448c769bbfbd20137c465c
Liraalkeyn <mikunar2018@gmail.com>
2023-11-21 22:49:42 +0300

    Refactoring

--- src/main/java/com/example/testsBack/controllers/QuestGroupController.java
+++ src/main/java/com/example/testsBack/controllers/QuestGroupController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/QuestGroups")
+@RequestMapping("/questGroups")
 public class QuestGroupController {
     @Autowired
     private QuestGroupService questGroupService;
